6b0edef9c65210ba640d66f3729bad9549b1c5c9
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-09-28 14:29:19 +0700

    Revert "X01AD: Replace audio@2.0-service with audio.service"
    This reverts commit 7c22168ce022c175d8d43663c37937b12e16cd8d.

--- device.mk
+++ device.mk
@@ -78,9 +78,10 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@5.0-impl \
+    android.hardware.audio@2.0-service \
+    android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@2.0-impl \
-    android.hardware.audio.service \
     audio.a2dp.default \
     audio.primary.msm8953 \
     audio.r_submix.default \
