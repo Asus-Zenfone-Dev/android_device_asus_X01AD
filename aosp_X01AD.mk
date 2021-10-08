ca55f7ea6482bb8ffa0046efeb1a7d8941288025
nguyenhung9x2020 <nguyenhung9x2018@gmail.com>
2021-10-08 07:10:12 +0700

    X01AD: AOSP makefile

--- aosp_X01AD.mk
+++ aosp_X01AD.mk
@@ -5,9 +5,12 @@
 #
 
 # Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_product.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_o_mr1.mk)
+$(call inherit-product-if-exists, vendor/build/config/BoardConfigAOSP.mk)
 
 # Inherit from onclite device
 $(call inherit-product, device/asus/X01AD/device.mk)
